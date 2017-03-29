426256a05f70c0593bad2fb6b338449ebdcd3206
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-03-29 13:23:59 -0600

    creando lista y botones para elimitar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-#Proyecto Final Sprint 2
-*primero estructure el index.html, le damos la primer maquetacion.
+#!Proyecto Final Sprint 2
+1. primero estructure el index.html, le damos la primer maquetacion.
+2. creando un article, en tonde contendra los elementos de la lista
+3. Creando la lista, y el evento onclick de los botones, para añadir los pendientes.
+4. creando los botones de la lista para elimiarlos.
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,73 @@
+function Lista(titulo,contenido){
+  this.titulo = titulo;
+  this.contenido=contenido;
+}
 
+var contador=0;
+function crearLista(){
+  contador++;
+
+  var titulo = document.getElementById("titulo").value;
+  var contenedor = document.getElementById("contenedor");
+  //contenedor.id="contenedor"+contador;
+  var botonX =document.createElement("button");
+  botonX.id="XX"+contador;
+  botonX.setAttribute("onclick","remover();")
+
+  var tr=document.createElement("tr");
+  tr.id="tr"+contador;
+
+  var td=document.createElement("td");
+  td.id="td"+contador;
+
+  var article =document.createElement("article");
+  article.id="article"+contador;
+
+  var h2 =document.createElement("h2");
+  h2.id="titulo"+contador;
+
+  var ul =document.createElement("ul")
+  ul.id="lista"+contador;
+
+  //var li =document.createElement("li");
+  //li.id="elementoLista"+contador;
+
+  var input=document.createElement("input");
+  input.id="entradalista"+contador;
+  var boton =document.createElement("button");
+  boton.id="boton"+contador;
+  boton.setAttribute("onclick","crearPendiente();")
+  //boton.onclick="crearPendiente()";
+  console.log(contenedor);
+  contenedor.appendChild(tr);
+  tr.appendChild(td);
+  td.appendChild(article)
+  article.appendChild(h2);
+  article.appendChild(ul);
+  //ul.appendChild(li);
+  article.appendChild(input);
+  article.appendChild(boton).innerHTML="Crear Pendiente"
+  tr.appendChild(botonX).innerHTML="x"
+  h2.innerText=titulo;
+
+  document.getElementById("titulo").value='';
+}
+
+function crearPendiente(){
+  var elemento =document.getElementById("entradalista"+contador).value;
+  var ul =document.getElementById("lista"+contador);
+  var li=document.createElement("li");
+  var boton =document.createElement("button");
+  boton.id="X"+contador;
+  boton.setAttribute("onclick","remover();")
+  ul.appendChild(li);
+
+  li.innerText=elemento
+  ul.appendChild(boton).innerHTML="X";
+  document.getElementById("entradalista"+contador).value='';
+
+}
+
+function removerElemento(){
+  
+}
